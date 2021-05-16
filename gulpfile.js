57e227c0de8af33c087a579931b4c09be630391a
Beth <Beth3346@gmail.com>
2021-05-16 14:01:23 -0500

    update icon hamburger

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 const gulp = require('gulp');
-const path = require('path');
 const sass = require('gulp-sass');
-// const scsslint = require('gulp-scss-lint');
 const plumber = require('gulp-plumber');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const browsersync = require('browser-sync').create();
 const del = require('del');
 
 var paths = {
@@ -13,23 +10,6 @@ var paths = {
   css: 'dist/css/',
   images: 'dist/images/'
 };
-
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: './dist/'
-    },
-    port: 3000
-  });
-  done();
-}
-
-// BrowserSync Reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
 
 // Clean assets
 function clean() {
@@ -45,33 +25,6 @@ function copyIndex() {
 function copyImages() {
   return gulp.src('./images/**/*.{gif,jpg,png,svg}')
     .pipe(gulp.dest(paths.images))
-}
-
-// Watch files
-function watchFiles(done) {
-  copyImages();
-
-  gulp.watch(['./**/*.scss', 'dist/index.html'], () => {
-    return gulp
-      .src('./*.scss')
-      .pipe(plumber())
-      .pipe(
-        sass({
-          includePaths: ['node_modules']
-        }).on('error', sass.logError)
-      )
-      .pipe(gulp.dest(paths.css))
-      .pipe(
-        autoprefixer({
-          cascade: false
-        })
-      )
-      .pipe(gulp.dest(paths.css))
-      .pipe(cleanCSS({ debug: true }))
-      .pipe(gulp.dest(paths.css))
-      .pipe(copyIndex())
-      .pipe(browserSyncReload(done));
-  });
 }
 
 gulp.task('default', () => {
@@ -97,9 +50,6 @@ gulp.task('default', () => {
     .pipe(copyIndex());
 });
 
-const watch = gulp.parallel(copyIndex, copyImages, watchFiles, browserSync);
-
 exports.clean = clean;
-exports.watch = watch;
 exports.copyIndex = copyIndex;
 exports.copyImages = copyImages;
