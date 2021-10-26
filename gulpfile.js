d042e55c727a8308c3af05c0d0dbe8d4ee38fa0b
Beth <Beth3346@gmail.com>
2021-10-25 22:39:43 -0500

    remove to-rems

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,47 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const del = require('del');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
 
 var paths = {
-  app: 'dist/',
-  css: 'dist/css/',
-  images: 'dist/images/'
+  app: "dist/",
+  css: "dist/css/",
+  images: "dist/images/",
 };
 
 // Clean assets
 function clean() {
-  return del(['./dist/css/', './dist/images/', './dist/index.html']);
+  return del(["./dist/css/", "./dist/images/", "./dist/index.html"]);
 }
 
 // Copy HTML
 function copyIndex() {
-  return gulp.src('./index.html')
-    .pipe(gulp.dest(paths.app))
+  return gulp.src("./index.html").pipe(gulp.dest(paths.app));
 }
 
 function copyImages() {
-  return gulp.src('./images/**/*.{gif,jpg,png,svg}')
-    .pipe(gulp.dest(paths.images))
+  return gulp
+    .src("./images/**/*.{gif,jpg,png,svg}")
+    .pipe(gulp.dest(paths.images));
 }
 
-gulp.task('default', () => {
+gulp.task("default", () => {
   copyImages();
 
   return gulp
-    .src('./*.scss')
+    .src("./*.scss")
     .pipe(plumber())
     .pipe(
       sass({
-        includePaths: ['node_modules']
-      }).on('error', sass.logError)
+        includePaths: ["node_modules"],
+      }).on("error", sass.logError)
     )
     .pipe(gulp.dest(paths.css))
     .pipe(
       autoprefixer({
-        cascade: false
+        cascade: false,
       })
     )
     .pipe(gulp.dest(paths.css))
